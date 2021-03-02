c444ecf0da0a9ca78b64070d1d9002ff5d371144
marat2183 <marat.grig318@gmail.com>
2021-03-02 04:11:45 +0300

    'final_version'

--- Magma_cipher_final.py
+++ Magma_cipher_final.py
@@ -3,13 +3,14 @@ import random
 import string
 from binascii import hexlify, unhexlify
 from typing import Union
+import time
+
+with open('S_blocks_matrix.json') as f:
+    data = json.load(f)
+s_blocks = data['matrix']
 
 
-
-
-
-
-def get_textblocks(text: str, size: int) -> list:
+def get_text_blocks(text: str, size: int) -> list:
     count = 0
     text_blocks = []
     for i in range(len(text)):
@@ -49,7 +50,7 @@ def text_decode_to_binary(text: Union[str, int], operation: str) -> str:
     return bin_str
 
 
-def xor_dif_parts(left_part: str, right_part: str) -> str:
+def xor(left_part: str, right_part: str) -> str:
     xor_result = ''
     for left_bit, right_bit in zip(left_part, right_part):
         xor_result += str(int(left_bit) ^ int(right_bit))
@@ -59,61 +60,52 @@ def xor_dif_parts(left_part: str, right_part: str) -> str:
 def get_round_keys(key: str) -> list:
     key = text_decode_to_binary(key, 'key')
     key = padding(key, 'key', 256)
-    round_keys = get_textblocks(key, 32)
-    return round_keys
+    round_keys = get_text_blocks(key, 32)
+    return round_keys * 3 + list(reversed(round_keys))
 
 
 def get_s_blocks_inputs(bin_data: str) -> list:
-    S_block_inputs = get_textblocks(bin_data, 4)
-    return S_block_inputs
+    s_block_inputs = get_text_blocks(bin_data, 4)
+    return s_block_inputs
 
 
 def mod_32(text_block: str, round_key: str) -> str:
     first_number = int(text_block, 2)
     second_number = int(round_key, 2)
-    sum = first_number + second_number
-    if sum < 2 ** 32:
-        result = bin(sum)[2:].zfill(32)
-        return result
-    else:
-        temp = sum - 2 ** 32
-        result = bin(temp)[2:].zfill(32)
-        return result
+    temp = first_number + second_number
+    result = bin(temp % 2 ** 32)[2:].zfill(32)
+    return result
 
 
-def cyclic_shift_to_left(data: str, number: int) -> str:
-    result = data[number:] + data[0:number]
+def cyclic_shift_to_left(text: str, number: int) -> str:
+    result = text[number:] + text[0:number]
     return result
 
 
 def f_function(text_block: str, round_key: str) -> str:
-    # with open('S_blocks_matrix.json') as f:
-    #     data = json.load(f)
-    global S_blocks
-    # S_blocks = data['matrix']
     mod_32_result = mod_32(text_block, round_key)
-    S_blocks_inputs = get_s_blocks_inputs(mod_32_result)
+    s_blocks_inputs = get_s_blocks_inputs(mod_32_result)
     result = ''
-    for i in range(len(S_blocks_inputs)):
-        col = int(S_blocks_inputs[i], 2)
+    for i in range(len(s_blocks_inputs)):
+        col = int(s_blocks_inputs[i], 2)
         row = i
-        temp = bin(S_blocks[row][col])[2:].zfill(4)
+        temp = bin(s_blocks[row][col])[2:].zfill(4)
         result += temp
     result = cyclic_shift_to_left(result, 11)
     return result
 
 
-def feistel_scheme(text_block: str, round_keys: list, round_keys_queue: list, rounds_number: int):
+def feistel_scheme(text_block: str, round_keys: list, rounds_number: int) -> str:
     text_part = ''
-    left_part, right_part = get_textblocks(text_block, 32)
+    left_part, right_part = get_text_blocks(text_block, 32)
     for i in range(rounds_number):
         if i == 31:
-            f_function_result = f_function(right_part, round_keys[round_keys_queue[i]])
-            xor_result = xor_dif_parts(left_part, f_function_result)
+            f_function_result = f_function(right_part, round_keys[i])
+            xor_result = xor(left_part, f_function_result)
             text_part = xor_result + right_part
         else:
-            f_function_result = f_function(right_part, round_keys[round_keys_queue[i]])
-            xor_result = xor_dif_parts(left_part, f_function_result)
+            f_function_result = f_function(right_part, round_keys[i])
+            xor_result = xor(left_part, f_function_result)
             left_part, right_part = right_part, xor_result
     return text_part
 
@@ -125,30 +117,28 @@ def decode_from_bin_to_hex(bin_str: str) -> Union[str, bytes]:
 
 
 def encode(plaintext: str, key: Union[str, int]) -> str:
-    ciphertext = ''
+    cipher_text = ''
     n = 32
     bin_plaintext = text_decode_to_binary(plaintext, 'encode')
     bin_plaintext = padding(bin_plaintext, 'plaintext', 64)
-    plaintext_blocks = get_textblocks(bin_plaintext, 64)
+    plaintext_blocks = get_text_blocks(bin_plaintext, 64)
     for plaintext_block in plaintext_blocks:
         round_keys = get_round_keys(key)
-        round_keys_queue = [0, 1, 2, 3, 4, 5, 6, 7] * 3 + [7, 6, 5, 4, 3, 2, 1, 0]
-        ciphertext_part = feistel_scheme(plaintext_block, round_keys, round_keys_queue, n)
-        ciphertext += ciphertext_part
-    return decode_from_bin_to_hex(ciphertext)
+        cipher_text_part = feistel_scheme(plaintext_block, round_keys, n)
+        cipher_text += cipher_text_part
+    return decode_from_bin_to_hex(cipher_text)
 
 
-def decode(ciphertext: str, key: Union[str, int]) -> str:
+def decode(cipher_text: str, key: Union[str, int]) -> str:
     plaintext = ''
     n = 32
-    ciphertext = text_decode_to_binary(ciphertext, 'decode')
-    ciphertext = padding(ciphertext, 'ciphertext', 64)
-    ciphertext_blocks = get_textblocks(ciphertext, 64)
-    for ciphertext_block in ciphertext_blocks:
-        round_keys_queue = [0, 1, 2, 3, 4, 5, 6, 7] * 3 + [7, 6, 5, 4, 3, 2, 1, 0]
-        round_keys_queue.reverse()
+    cipher_text = text_decode_to_binary(cipher_text, 'decode')
+    cipher_text = padding(cipher_text, 'cipher_text', 64)
+    cipher_text_blocks = get_text_blocks(cipher_text, 64)
+    for cipher_text_block in cipher_text_blocks:
         round_keys = get_round_keys(key)
-        plaintext_part = feistel_scheme(ciphertext_block, round_keys, round_keys_queue, n)
+        round_keys.reverse()
+        plaintext_part = feistel_scheme(cipher_text_block, round_keys, n)
         plaintext += plaintext_part
     hex_result = decode_from_bin_to_hex(plaintext)
     plaintext = unhexlify(hex_result).decode()
@@ -171,46 +161,44 @@ def tests(n: int, text_len: int) -> bool:
     return success
 
 
-with open('S_blocks_matrix.json') as f:
-    data = json.load(f)
-S_blocks = data['matrix']
-
-mode = input('''
+def main():
+    mode = input('''
 Режим работы:
-1) Зашифровать
-2) Расшифровать
-Ответ:
-''')
-if mode == '1':
-    with open('plaintext.txt', 'r', encoding='utf-8') as f:
-        pt = f.read()
-    with open('key.txt', 'r', encoding='utf-8') as f:
-        key = int(f.read())
-    try:
-        result = encode(pt, key)
-        print('ciphertext:', result, sep='\n')
-        with open('ciphertext.txt', 'w', encoding='utf-8') as f:
-            f.write(result)
-    except Exception as e:
-       print(str(e))
-elif mode == '2':
-    with open('ciphertext.txt', 'r', encoding='utf-8') as f:
-        ct = f.read()
-    with open('key.txt', 'r', encoding='utf-8') as f:
-        key = int(f.read())
-    try:
-        result = decode(ct, key)
-        print('plaintext:', result, sep='\n')
-        with open('plaintext.txt', 'w', encoding='utf-8') as f:
-            f.write(result)
-    except Exception as e:
-        print(str(e))
+1) Encrypt
+2) Decrypt
+Answer: ''')
+
+    if mode == '1':
+        start_time = time.time()
+        with open('plaintext', 'r', encoding='utf-8') as f:
+            pt = f.read()
+        with open('key', 'r', encoding='utf-8') as f:
+            key = int(f.read())
+        try:
+            result = encode(pt, key)
+            print('ciphertext:', result, sep='\n')
+            with open('ciphertext', 'w', encoding='utf-8') as f:
+                f.write(result)
+        except Exception as e:
+            print(str(e))
+        print(f'time: {time.time() - start_time}')
+    elif mode == '2':
+        start_time = time.time()
+        with open('ciphertext', 'r', encoding='utf-8') as f:
+            ct = f.read()
+        with open('key', 'r', encoding='utf-8') as f:
+            key = int(f.read())
+        try:
+            result = decode(ct, key)
+            print('plaintext:', result, sep='\n')
+            with open('plaintext', 'w', encoding='utf-8') as f:
+                f.write(result)
+        except Exception as e:
+            print(str(e))
+        print(f'time: {(time.time() - start_time)}')
+    else:
+        print('Выбран несуществующий режим работы')
 
 
-if (tests(1, 1000)):
-    print('ok')
-
-#
-# a = encode('hello', 3456786514234234212312312312312312313123141241342413)
-# print(a)
-# print(decode(a, 3456786514234234212312312312312312313123141241342413))
+if __name__ == '__main__':
+    main()
